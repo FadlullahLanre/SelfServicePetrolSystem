2b79f3bf1acb5fe6f21fb08f9215a591d54ebd23
Fadlullah Lanre <Fadlullahlanre@gmail.com>
2022-06-07 16:22:08 +0100

    Added emergency fix to User entity

--- SelfServicePump/Entities/User.cs
+++ SelfServicePump/Entities/User.cs
@@ -28,6 +28,8 @@ namespace SelfServicePump.Entities
         [Required]
         public string LastName { get; set; }
         [Required]
+        public string Address{ get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
         [Required]
         public DateTime CreatedAt { get; set; }
